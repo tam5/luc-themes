bc4a601c9224d465797d532fa2fcf6a84e530394
tam5 <arimiller92@gmail.com>
2018-11-11 13:40:37 -0500

    add google theme

---
+++ themes/luc-google-theme.el
@@ -0,0 +1,36 @@
+;;; luc-material-theme.el --- luc version of koalin-aurora-theme
+;;; Commentary:
+
+;;; Code:
+(require 'luc-themes)
+
+(define-luc-theme google "Hey Google Theme."
+  (
+    (background-color "#ffffff")
+    (far-background-color "#ffffff")
+    (foreground-color "#DB4437")
+    (highlight-line-color "#eeeeee")
+    (selection-color "#ccc")
+    (cursor-color "#4285F4")
+    (subtle-color "#ddd")
+    (supporting-1-color "#333")
+    (supporting-2-color "#A6C4F4")
+
+    (comment-color "#A6C4F4")
+    (primitive-color "#DB4437")
+    (constant-color "#DB4437")
+    (function-color "#0F9D58")
+    (keyword-color "#4285F4")
+    (operator-color "#000")
+    (string-color "#F4B400")
+    (type-color "#F4B400")
+    (variable-color "#000")
+
+    (success-color "#0F9D58")
+    (warning-color "#F4B400")
+    (error-color "#DB4437")
+
+    (accent-1-color "#DB4437")
+    (accent-2-color "#0F9D58")))
+
+;;; luc-material-theme.el ends here
