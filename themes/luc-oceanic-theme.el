c9a03b7f2541158b968e8ae4e71bf4540caef66e
tam5 <arimiller92@gmail.com>
2019-04-04 11:58:39 -0400

    add oceanic

---
+++ themes/luc-oceanic-theme.el
@@ -0,0 +1,36 @@
+;;; luc-oceanic-theme.el --- luc version of oceanic-theme
+;;; Commentary:
+
+;;; Code:
+(require 'luc-themes)
+
+(define-luc-theme oceanic "Oceanic Theme."
+  (
+    (background-color     "#1B2B34")
+    (far-background-color "#1B2B34")
+    (foreground-color     "#D8DEE9")
+    (highlight-line-color "#2C3B45")
+    (selection-color      "#C594C5")
+    (cursor-color         "#4F5B66")
+    (subtle-color         "#343D46")
+    (supporting-1-color   "#A7ADBA")
+    (supporting-2-color   "#C0C5CE")
+
+    (comment-color        "#4F5B66")
+    (primitive-color      "#AB7967")
+    (constant-color       "#EC5f67")
+    (function-color       "#6699CC")
+    (keyword-color        "#C594C5")
+    (operator-color       "#89ddff")
+    (string-color         "#99C794")
+    (type-color           "#FAC863")
+    (variable-color       "#F99157")
+
+    (success-color        "SeaGreen2")
+    (warning-color        "#f78a64")
+    (error-color          "#ff5370")
+
+    (accent-1-color       "#EC5f67")
+    (accent-2-color       "#99C794")))
+
+;;; luc-oceanic-theme.el ends here
